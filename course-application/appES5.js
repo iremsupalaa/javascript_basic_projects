0b5461d3cb52bb0d13603548f321abb3c3e02a7a
iremsu.pala <iremsu.pala@nuteknoloji.com>
2026-03-20 23:47:44 +0300

    course application project has been converted to ES6.

--- course-application/appES5.js
+++ course-application/appES5.js
@@ -1,63 +1,56 @@
-//Course constructor
+// Course constructor
 function Course(title,instructor ,image){
     this.title = title;
     this.instructor = instructor;
     this.image = image;
 }
 
-//UI constructor
-function UI() {
-
+// UI constructor
+function UI(){
 }
 
-UI.prototype.addCourseToList = function(course) {
+UI.prototype.addCourseToList = function(course){
     const list = document.getElementById('course-list');
 
-    var html = ` 
-       <tr>
-       <td><img src = "img/${course.image}"</td>
-       <td>${course.list}</td>
-       <td>${course.instructor}</td>
-       <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
-       </tr>
+    var html = `
+         <tr>
+            <td><img src="img/${course.image}"/></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+         </tr>    
     `;
 
     list.innerHTML += html;
-
 }
 
-UI.prototype.clearControls = function() {
-
-      const title = document.getElementById('title').value="";
+UI.prototype.clearControls = function(){
+    const title = document.getElementById('title').value="";
     const instructor = document.getElementById('instructor').value="";
     const image = document.getElementById('image').value="";
-
-
 }
 
-UI.prototype.deleteCourse = function(element) {
+UI.prototype.deleteCourse = function(element){
     if(element.classList.contains('delete')){
         element.parentElement.parentElement.remove();
-
-    } 
+    }
 }
 
-UI.prototype.showAlert = function(message, className) {
+UI.prototype.showAlert = function(message,className){
+    
     var alert = `
-       <div class= "alert alert-${className}">
-          ${message}
-       </div>
-
+        <div class="alert alert-${className}">
+            ${message}
+        </div>    
     `;
+
     const row = document.querySelector('.row');
-    //beforeBegin, afterBegin, beforeEnd, afterEnd
-    row.insertAdjacentHTML('beforeBegin', alert);
+    // beforeBegin , afterBegin , beforeEnd , afterEnd
+    row.insertAdjacentHTML('beforeBegin',alert);
 
-    setTimeout(() => {
-        document.querySelector('alert').remove();
+    setTimeout(()=>{
+        document.querySelector('.alert').remove();
     },3000);
-
-
 
 }
 
@@ -70,25 +63,26 @@ document.getElementById('new-course').addEventListener('submit',function(e){
     // create course object
     const course = new Course(title,instructor,image);
 
-    //create ui
+    // create UI
     const ui = new UI();
 
-    if(title === '' || instructor === '' || image === '') {
-        ui.showAlert('Please complete the form', 'warning');
-    }else {
-    ui.addCourseToList(course);
-    ui.clearControls();
-    ui.showAlert('the course has been added', 'success')
+    if(title==='' || instructor ==='' || image === ''){
+        ui.showAlert('Please complete the form','warning');
+    }else{
+        // add course to list
+        ui.addCourseToList(course); 
 
+        // clear controls
+        ui.clearControls();
+
+        ui.showAlert('the course has been added','success');
     }
 
     e.preventDefault();
 });
 
-document.getElementById('course-list').addEventListener('click', function(e){
-
+document.getElementById('course-list').addEventListener('click',function(e){
     const ui = new UI();
     ui.deleteCourse(e.target);
-    ui.showAlert('the course has been deleted', 'danger')
-
-})
+    ui.showAlert('the course has been deleted','danger');
+});
