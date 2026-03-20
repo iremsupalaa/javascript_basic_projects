0821845a94953a11f4cbbc8b1fb0dadbcd4e44f7
iremsu.pala <iremsu.pala@nuteknoloji.com>
2026-03-20 22:01:11 +0300

    added course-application project

---
+++ course-application/appES5.js
@@ -0,0 +1,13 @@
+document.getElementById('new-course').addEventListener('submit',
+    
+    function(e){
+        const title = document.getElementById('title').value;
+        const instructor = document.getElementById('instructor').value;
+        const image = document.getElementById('image');
+
+
+        e.preventDefault();
+
+
+
+});
