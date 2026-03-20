5f8278361fc7d3e0ed1ea15cbf0cd3917d0c686f
iremsu.pala <iremsu.pala@nuteknoloji.com>
2026-03-20 22:05:54 +0300

    added course-application project

--- course-application/appES5.js
+++ course-application/appES5.js
@@ -1,13 +1,24 @@
-document.getElementById('new-course').addEventListener('submit',
-    
-    function(e){
-        const title = document.getElementById('title').value;
-        const instructor = document.getElementById('instructor').value;
-        const image = document.getElementById('image');
+function Course(title,instructor ,image){
+    this.title = title;
+    this.instructor = instructor;
+    this.image = image;
+}
+
+document.getElementById('new-course').addEventListener('submit',function(e){
+
+    const title = document.getElementById('title').value;
+    const instructor = document.getElementById('instructor').value;
+    const image = document.getElementById('image').value;
+
+    // create course object
+    const course = new Course(title,instructor,image);
 
 
-        e.preventDefault();
+    console.log(course);
+    // save to database
+
+    // show on the ui
 
 
-
+    e.preventDefault();
 });
