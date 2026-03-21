bc8e50c5c79306f993d4ec362717fe1fe6e4f2fb
iremsu.pala <iremsu.pala@nuteknoloji.com>
2026-03-21 22:47:47 +0300

    added music-player project

--- music-player/app.js
+++ music-player/app.js
@@ -1,8 +1,30 @@
+const container = document.querySelector(".container");
+const image = document.querySelector("#music-image");
+const title = document.querySelector("#music-details .title");
+const singer = document.querySelector("#music-details .singer");
+const prev = document.querySelector("#controls #prev");
+const play = document.querySelector("#controls #play");
+const next = document.querySelector("#controls #next");
+
 const player = new MusicPlayer(musicList);
 
-let music= player.getMusic();
-console.log(music.getName());
+window.addEventListener("load", () => {
+    let music = player.getMusic();
+    displayMusic(music);
+});
 
+function displayMusic(music) {
+    title.innerText = music.getName();
+    singer.innerText = music.singer;
+    image.src = "img/" + music.img;
+    audio.src = "mp3/" + music.file;
+}
+
+play.addEventListener("click", () => {
+    audio.play();
+})
+
+/* TEST
 player.next();
 music = player.getMusic();
 console.log(music.getName());
@@ -16,5 +38,5 @@ console.log(music.getName());
 
 player.next();
 music = player.getMusic();
-console.log(music.getName());
+console.log(music.getName());*/
 
